ea53a17b97e4e1b77dff482788ec6132ae9c041e
William Monzón <wmonzonl1@miumg.edu.gt>
2026-02-20 18:20:23 -0600

    Ingreso del programa Dia de la Semana

---
+++ VariablesYOperadoresProyecto/src/Ejercicios/DiaSemana.java
@@ -0,0 +1,44 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class DiaSemana {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int numero;
+		
+		System.out.println("Ingrese un número del 1 al 7");
+		numero = sc.nextInt();
+		
+		switch(numero) {
+			case 1:
+				System.out.println("Lunes");
+				break;
+			case 2:
+				System.out.println("Martes");
+				break;
+			case 3:
+				System.out.println("Miércoles");
+				break;
+			case 4:
+				System.out.println("Jueves");
+				break;
+			case 5:
+				System.out.println("Viernes");
+				break;
+			case 6:
+				System.out.println("Sábado");
+				break;
+			case 7:
+				System.out.println("Domingo");
+				break;
+			default:
+				System.out.println("Número inválido");
+		}
+		
+	}
+
+}
